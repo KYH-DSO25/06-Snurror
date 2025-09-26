9ec7a01a62ca7dda5856fd0d9e41540125b064c7
Claes Engelin <claes.engelin@zocom.se>
2025-09-26 11:56:15 +0200

    Efter uppgift 8

--- 07 Fibonacci/Program.cs
+++ 07 Fibonacci/Program.cs
@@ -5,8 +5,27 @@
  *      Varje tal i serien (utom de två första) är summan av de två föregående
  */
 
+// Talet på plats nummer 1 är lika med 0
+using System.Numerics;
+
+Console.Write("Ange ett tal: ");
+uint n = uint.Parse(Console.ReadLine());
+
+BigInteger first = 0;
+BigInteger second = 1;
+BigInteger third = 0;
+BigInteger sum = 0;
 
 
+for (int i = 0; i < n - 1; i++)
+{
+    first = second;
+    second = third;
+    third = first + second;
+    sum = sum + third;
+}
+
+Console.WriteLine("\nSumman av de {0} första talen i Fibonacciserien = {1}\n\n", n, sum);
 
 
 Console.Write("\n\n\nTryck på en tangent för att stänga fönstret...");
--- 08 Största gemensamma nämnare/Program.cs
+++ 08 Största gemensamma nämnare/Program.cs
@@ -1,11 +1,27 @@
 ﻿/*
- * 8    Skriv ett program som beräknar den största gemensamma delaren till två givna tal. 
+ * 8    Skriv ett program som beräknar den största gemensamma nämnaren till två givna tal. 
  *      Använd Euklides algoritm (Googla!)
  *      
  *      På engelska heter det greatest common divisor, GCD
  */
 
+Console.WriteLine("Ange två tal: ");
+Console.Write("   1: "); uint a = uint.Parse(Console.ReadLine());
+Console.Write("   2: "); uint b = uint.Parse(Console.ReadLine());
 
+while (a != b)
+{
+    if (a > b)
+    {
+        a = a - b;
+    }
+    else
+    {
+        b = b - a;
+    }
+}
+
+Console.WriteLine("\nStörsta gemensamma nämnare: {0}", a);
 
 
 
